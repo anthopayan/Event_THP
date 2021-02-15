0a888b53f70c6aae2ee847c99ce5869ccf8fb372
anthopayan <anthony.payan08@outlook.fr>
2021-02-15 14:42:03 +0100

    Mail

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
     has_many :events
     has_many :participations
    has_many :events, through: :participations 
+
+    after_create :welcome_send
+  
+    def welcome_send
+      UserMailer.welcome_email(self).deliver_now
+    end
+  
+
 end
