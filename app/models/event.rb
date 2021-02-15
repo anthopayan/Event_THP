98ccc929645af8349cbea5acf4400312387ac148
sassok <kevin.sassoo@gmail.com>
2021-02-15 14:03:59 +0100

    Heroku push

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,19 +1,19 @@
 class Event < ApplicationRecord
-    validates :start_date, presence :true, 
+    validates :start_date, presence: true, 
         validate :start_of_event
 
-    validates :duration, presence :true, 
+    validates :duration, presence: true, 
         numericality: { only_integer: true, greater_than: 0 },
         validate :multiple_of_5
     
-    validates :title, presence :true, length: { in: 5..140 }
+    validates :title, presence: true, length: { in: 5..140 }
     validates :description, presence :true, length: { in: 20..1000 }
 
-    validates :price, presence :true, 
+    validates :price, presence: true, 
         numericality: {only_integer: true, greater_than_or_equal_to: 1, 
         less_than_or_equal_to: 1000}
 
-    validates :location, presence :true
+    validates :location, presence: true
 
     has_many :participations
     has_many :users, through: :participations
@@ -23,7 +23,7 @@ class Event < ApplicationRecord
     
 
     def multiple_of_5
-        if duration != nil && duration %5 != 0
+        if duration != nil && (duration % 5) != 0
             errors.add(:duration, "ERROR duration is not %5 !")
         end
     end
