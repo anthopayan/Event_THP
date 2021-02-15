9ae7ed0a509953ce0d2c994d872edfcfa22dac21
volpito <mpj.rigot@gmail.com>
2021-02-15 12:18:41 +0100

    validations def

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,37 @@
 class Event < ApplicationRecord
-    validates :start_date, presence :true, numericality: { only_integer: :greater_than_or_equal_to{Time.now}}
+    validates :start_date, presence :true, 
+        validate :start_of_event
+
     validates :duration, presence :true, 
+        numericality: { only_integer: true, greater_than: 0 },
+        validate :multiple_of_5
+    
     validates :title, presence :true, length: { in: 5..140 }
     validates :description, presence :true, length: { in: 20..1000 }
-    validates :price, presence :true, :inclusion => 1..1000
+
+    validates :price, presence :true, 
+        numericality: {only_integer: true, greater_than_or_equal_to: 1, 
+        less_than_or_equal_to: 1000}
+
     validates :location, presence :true
+
     has_many :participations
     has_many :users, through: :participations
     belongs_to :user
+
+
+    
+
+    def multiple_of_5
+        if duration != nil && duration %5 != 0
+            errors.add(:duration, "ERROR duration is not %5 !")
+        end
+    end
+
+    def start_of_event
+        if start_date != nil && Time.now < start_date
+            errors.add(:start_date, "ERROR date is impossible")
+        end
+    end
+
 end
