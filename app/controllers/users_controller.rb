ad63cd347aa3760af99d4e1cdca8988514f5aaa7
sassok <kevin.sassoo@gmail.com>
2021-02-16 22:46:41 +0100

    yep

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, :user
   def show
   
+  end
+
+  def user
+    @user = User.find(params['id'])
+    if current_user.id == @user.id
+    else
+      redirect_to root_path
+      flash[:danger]= "Bien essayé petit malin ! ;P"
+    end
   end
   
 end
