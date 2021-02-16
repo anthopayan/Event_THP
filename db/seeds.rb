a84901a05e2cdcc93292321f0a343f086ff210ba
volpito <mpj.rigot@gmail.com>
2021-02-16 14:47:03 +0100

    max

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
+Event.destroy_all
 
-User.create(email: "event.1.batte@yopmail.com")
-User.create(email: "event.2.batte@yopmail.com")
-User.create(email: "event.3.batte@yopmail.com")
-User.create(email: "event.4.batte@yopmail.com")
-User.create(email: "event.5.batte@yopmail.com")
+User.create(email: "event.1.batte@yopmail.com", password: Faker::Lorem.words(number: 7))
+User.create(email: "event.2.batte@yopmail.com", password: Faker::Lorem.words(number: 7))
+User.create(email: "event.3.batte@yopmail.com", password: Faker::Lorem.words(number: 7))
+User.create(email: "event.4.batte@yopmail.com", password: Faker::Lorem.words(number: 7))
+User.create(email: "event.5.batte@yopmail.com", password: Faker::Lorem.words(number: 7))
+
+Event.create(start_date:Faker::Date.forward(days: 10), duration: [5,10,20,30].sample, title: Faker::Games::Fallout.faction, description: Faker::Lorem.paragraph(sentence_count: 2), price: Faker::Number.between(from: 1, to: 1000),location: Faker::Movies::LordOfTheRings.location, admin_id: rand(1..5))
+Event.create(start_date:Faker::Date.forward(days: 10), duration: [5,10,20,30].sample, title: Faker::Games::Fallout.faction, description: Faker::Lorem.paragraph(sentence_count: 2), price: Faker::Number.between(from: 1, to: 1000),location: Faker::Movies::LordOfTheRings.location, admin_id: rand(1..5))
+Event.create(start_date:Faker::Date.forward(days: 10), duration: [5,10,20,30].sample, title: Faker::Games::Fallout.faction, description: Faker::Lorem.paragraph(sentence_count: 2), price: Faker::Number.between(from: 1, to: 1000),location: Faker::Movies::LordOfTheRings.location, admin_id: rand(1..5))
+Event.create(start_date:Faker::Date.forward(days: 10), duration: [5,10,20,30].sample, title: Faker::Games::Fallout.faction, description: Faker::Lorem.paragraph(sentence_count: 2), price: Faker::Number.between(from: 1, to: 1000),location: Faker::Movies::LordOfTheRings.location, admin_id: rand(1..5))
+Event.create(start_date:Faker::Date.forward(days: 10), duration: [5,10,20,30].sample, title: Faker::Games::Fallout.faction, description: Faker::Lorem.paragraph(sentence_count: 2), price: Faker::Number.between(from: 1, to: 1000),location: Faker::Movies::LordOfTheRings.location, admin_id: rand(1..5))
