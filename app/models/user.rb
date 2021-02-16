a54b4df3e72952ba8548751e420a6a3e0effacb8
sassok <kevin.sassoo@gmail.com>
2021-02-16 16:11:54 +0100

    make it better

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,11 @@ class User < ApplicationRecord
     has_many :participations
    has_many :events, through: :participations 
 
-    after_create :welcome_send
+    #after_create :welcome_send
   
-    def welcome_send
-      UserMailer.welcome_email(self).deliver_now
-    end
+    #def welcome_send
+      #UserMailer.welcome_email(self).deliver_now
+    #end
   
 
 end
