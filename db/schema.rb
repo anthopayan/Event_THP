e2ba880d9a3a2c814241eeac7ced1aa27c86f32d
sassok <kevin.sassoo@gmail.com>
2021-02-16 10:27:08 +0100

    Start Devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,42 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_15_103151) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "events", force: :cascade do |t|
-    t.datetime "start_date"
-    t.integer "duration"
-    t.string "title"
-    t.text "description"
-    t.integer "price"
-    t.string "location"
-    t.bigint "admin_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["admin_id"], name: "index_events_on_admin_id"
-  end
-
-  create_table "participations", force: :cascade do |t|
-    t.string "stripe_customer_id"
-    t.bigint "user_id"
-    t.bigint "event_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["event_id"], name: "index_participations_on_event_id"
-    t.index ["user_id"], name: "index_participations_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
